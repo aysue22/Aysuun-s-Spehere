e16e3c5a94fc70360aa917c5fe75e352cb1da6b5
aysue22 <180599787+aysue22@users.noreply.github.com>
2026-07-02 23:14:46 +0300

    updated

--- src/consts.ts
+++ src/consts.ts
@@ -2,4 +2,4 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'The Coven Journal';
-export const SITE_DESCRIPTION = 'Welcome to my Journal!';
+export const SITE_DESCRIPTION = 'Toplumsal cinsiyet, ekoloji ve siyaset üzerine sosyolojik denemeler ve eleştirel incelemeler.';
